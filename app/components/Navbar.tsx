c0c59b1350fb59a7e52e21728c3575d77371211d
Jean Bosco B <jeanboscob77@gmail.com>
2025-09-16 18:27:37 +0200

    some update main in navbar component

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,7 +5,7 @@ import { usePathname } from "next/navigation";
 import { useState } from "react";
 import { Home, Info, Briefcase, Folder, Phone, Menu, X } from "lucide-react";
 import Image from "next/image";
-import Logo from "@/public/Bosconnect_Logo.png";
+import Logo from "@/public/Bosconnect Logo.png";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -20,17 +20,18 @@ export default function Navbar() {
   ];
 
   return (
-    <header className="bg-white shadow-md sticky top-0 z-50">
+    <header className="bg-gradient-to-r from-blue-700 to-blue-500 text-white  sticky top-0 z-50">
       <div className="container mx-auto flex items-center justify-between p-4">
         {/* Logo + Brand */}
         <Link href="/" className="flex items-center gap-2">
           <Image
+            className="rounded-full"
             src={Logo} // put logo in /public folder
             alt="Bosconnect Logo"
             width={60}
             height={60}
           />
-          <span className="text-lg font-bold text-blue-700">
+          <span className="text-lg font-bold text-gray-200">
             Bosconnect Ltd
           </span>
         </Link>
@@ -43,8 +44,8 @@ export default function Navbar() {
               href={href}
               className={`flex items-center gap-1 px-2 pb-1 border-b-2 transition ${
                 pathname === href
-                  ? "border-blue-600 text-blue-700 font-semibold"
-                  : "border-transparent text-gray-600 hover:text-blue-600 hover:border-blue-300"
+                  ? "border-white text-gray-100 font-semibold"
+                  : "border-transparent text-gray-100 hover:text-white hover:bg-blue-400 hover:rounded-b-sm hover:rounded-t-sm hover:border-gray-300 transition duration-500"
               }`}
             >
               {icon}
@@ -55,7 +56,7 @@ export default function Navbar() {
 
         {/* Mobile Menu Button */}
         <button
-          className="md:hidden text-blue-700 focus:outline-none"
+          className="md:hidden text-white focus:outline-none"
           onClick={() => setIsOpen(!isOpen)}
         >
           {isOpen ? <X size={28} /> : <Menu size={28} />}
@@ -64,14 +65,14 @@ export default function Navbar() {
 
       {/* Mobile Dropdown */}
       {isOpen && (
-        <nav className="md:hidden bg-blue-50 px-4 pb-4 space-y-2">
+        <nav className="md:hidden bg-blue-500 px-4 pb-4 space-y-2">
           {navLinks.map(({ href, label, icon }) => (
             <Link
               key={href}
               href={href}
               className={`flex items-center gap-2 p-2 rounded-md transition ${
                 pathname === href
-                  ? "bg-blue-100 text-blue-700 font-semibold"
+                  ? "bg-blue-300 text-white font-semibold"
                   : "hover:bg-blue-200"
               }`}
               onClick={() => setIsOpen(false)}
